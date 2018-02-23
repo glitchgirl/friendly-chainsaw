3f4b95826c6da90d162986717398dceb7851fe20
Morgan Smith <MSMITH1270@augusta.edu>
2018-02-23 11:34:51 -0500

    Add project files.

---
+++ joesGolfSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(joesGolfSite.Startup))]
+namespace joesGolfSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
