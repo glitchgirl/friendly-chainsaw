b416b7e1cdf9013a86ede1c1d05356adfc8f7cd7
Morgan Smith <MSMITH1270@augusta.edu>
2018-03-02 09:27:20 -0500

    added logos, stuff hits database, some minor ui changes

--- joesGolfSite/Controllers/HomeController.cs
+++ joesGolfSite/Controllers/HomeController.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using joesGolfSite.Models;
+using System.Data.SqlClient;
+using System.Data.Entity.Infrastructure;
+using joesGolfSite.Models.ViewModels;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+
 namespace joesGolfSite.Controllers
 {
     public class HomeController : Controller
@@ -31,7 +37,67 @@ namespace joesGolfSite.Controllers
         {
             var model = new Registerer();
             model = data;
+            Create(data);
             return View("About",model);
         }
+        [HttpPost]
+        public ActionResult LoadPage(AdminDisplayViewModel model)
+        {
+            if (model.AdminCode == "RSI")
+            {
+                Read();
+                return View("Results");
+            }
+            else
+            {
+                return View("Contact");
+            }
+        }
+        //public ActionResult Results(AdminDisplayResultsViewModel model)
+        //{
+        //    Read();
+        //    return View("Results", model);
+        //}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FirstName, LastName, EmailAddress, Phone, TeamName, ShirtSize, Teammembers")]Registerer register)
+        {
+           var db = new GolfSiteDbContext();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Registerers.Add(register);
+                    db.SaveChanges();
+                    return  View("About");
+                }
+            }
+            catch (RetryLimitExceededException )
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            }
+            return View("About");
+        }
+        public ActionResult Read()
+        {
+            var db = new GolfSiteDbContext();
+            var model = new AdminDisplayResultsViewModel();
+            try
+            {
+               // var results = db.Registerers.AsEnumerable().ToString();
+                var results =JsonConvert.DeserializeObject(JsonConvert.SerializeObject(db.Registerers.AsEnumerable()));
+                var example1Model = new JavaScriptSerializer().Deserialize<Registerer>(JsonConvert.SerializeObject(db.Registerers.AsEnumerable()).ToString());
+                model.DisplayResults = "";
+
+                var blah = "no";
+                
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            }
+            return View("Results", model);
+        }
+
     }
 }
---
+++ joesGolfSite/Models/ViewModels/AdminDisplayViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace joesGolfSite.Models.ViewModels
+{
+    public class AdminDisplayViewModel
+    {
+        public AdminDisplayViewModel() { }
+        public string AdminCode { get; set; }
+
+    }
+}
