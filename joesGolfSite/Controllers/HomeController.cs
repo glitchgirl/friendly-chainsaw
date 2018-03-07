c741f52e6f041082304982079b7304c5ab50398c
Morgan Smith <MSMITH1270@augusta.edu>
2018-03-07 12:55:08 -0500

    Revert "redoing front page"
    This reverts commit 46844635c544bb3f3f3aa0e30e5f1f5625cfaac9.

--- joesGolfSite/Controllers/HomeController.cs
+++ joesGolfSite/Controllers/HomeController.cs
@@ -21,11 +21,23 @@ namespace joesGolfSite.Controllers
             return View();
         }
 
-        public ActionResult RegistererList()
+        public ActionResult Teams()
         {
             return View();
         }
-     
+
+        [HttpPost]
+        public ActionResult LoadPage(string AdminCode)
+        {
+            if (AdminCode == "48DD25BF-84E6-4D73-AAFD-BEBC6D9DD729")
+            {
+                var registerers = dbgolf.Registerers;
+                return View("RegistererList", registerers);
+            }
+            return View("Teams");
+
+        }
+      
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Register(Registerer register)
